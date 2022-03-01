065e83a3c6c4b17c932a833f8722bf2e1523d09f
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-03-01 17:41:51 +0000

    e

--- deno_test/test.ts
+++ deno_test/test.ts
@@ -1,6 +1,7 @@
 const coreString = Deno.args[0];
 const githubString = Deno.args[1];
-
+console.log(coreString);
+console.log(githubString)
 const core = JSON.parse(coreString);
 const github = JSON.parse(githubString);
 
