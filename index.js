c05577c0805227e560293d581e14d650ca9df1cb
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-03-01 17:21:27 +0000

    feat: init

---
+++ index.js
@@ -0,0 +1,23 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { spawn } = require("child_process");
+
+try {
+  console.log(`Hello from NodeJS!`);
+  const denoProcess = spawn("deno", ["run", "deno_test/test.ts"]);
+
+
+  denoProcess.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+  
+  denoProcess.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+  
+  denoProcess.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+} catch (error) {
+  core.setFailed(error.message);
+}
